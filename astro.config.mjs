60c0b5b3f676aaac06460a889e7a3f0c5c81ed52
Mehdi Merkachi <merkachimehdi@gmail.com>
2025-02-02 13:58:05 +0100

    Updated to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   prefetch: true,
-  integrations: [tailwind()],
 
   markdown: {
     shikiConfig: {
       theme: "slack-dark",
       wrap: true,
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
